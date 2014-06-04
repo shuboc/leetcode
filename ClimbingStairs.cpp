45a8cc9fcfcdcdb6d5e1509fb9e78dd0edceeebf
Chris Chao <apiggy0204@gmail.com>
2014-06-04 23:55:16 +0800

    13/151.

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+            
+        int a = 1;
+        int b = 2;
+        for (int i=3; i<=n; i++) {
+            int tmp = b;
+            b = a + b;
+            a = tmp;
+        }
+        return b;
+    }
+};
