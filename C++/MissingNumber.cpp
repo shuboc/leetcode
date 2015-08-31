c452fc7c328ca5c1e0ad7b54a14c9b6490554b2a
Chris Chao <chris_chao@trend.com.tw>
2015-08-31 23:13:53 +0800

    Update 268 with bit operation solution

--- C++/MissingNumber.cpp
+++ C++/MissingNumber.cpp
@@ -16,3 +16,15 @@ public:
         return nums.size();
     }
 };
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int num = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            num ^= (nums[i] ^ (i+1));
+        }
+
+        return num;
+    }
+};
