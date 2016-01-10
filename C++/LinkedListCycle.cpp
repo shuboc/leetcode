d0ec338a4bd2b8ced1816668af64c61d6b25c180
Chris Chao <chris_chao@trend.com.tw>
2016-01-11 01:02:26 +0800

    148/310

--- C++/LinkedListCycle.cpp
+++ C++/LinkedListCycle.cpp
@@ -6,21 +6,19 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head) return false;
-        ListNode *slow = head->next;
-        if (!slow) return false;
-        ListNode *fast = slow->next;
-        if (!fast) return false;
-        while (slow && fast) {
-            if (slow == fast) return true;
+        if (!head || !head->next) return false;
+
+        ListNode *slow = head->next, *fast = head->next->next;
+
+        while (fast) {
+            if (fast == slow) return true;
             slow = slow->next;
-            if (fast->next) fast = fast->next->next;
-            else return false;
+            fast = fast->next ? fast->next->next : fast->next;
         }
+
         return false;
     }
 };
